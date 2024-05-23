724aa68de5f80d2177dddf6e649c98074094fa90
Waya-mst <kameeeeei1234@gmail.com>
2024-05-23 18:22:42 +0900

    implemented emitted-material

--- rayt/rayt.cpp
+++ rayt/rayt.cpp
@@ -26,7 +26,27 @@ namespace rayt {
     class Material {
     public:
         virtual bool scatter(const Ray& r, const HitRec& hrec, ScatterRec& srec) const = 0;
+        virtual vec3 emitted(const Ray& r, const HitRec& hrec) const { return vec3(0); }
     };
+
+    class DiffuseLight : public Material {
+    public:
+        DiffuseLight(const TexturePtr& emit)
+            : m_emit(emit) {
+        };
+
+        virtual bool scatter(const Ray& r, const HitRec& hrec, ScatterRec& srec) const override {
+            return false;
+        }
+
+        virtual vec3 emitted(const Ray& r, const HitRec& hrec) const override {
+            return m_emit->value(hrec.u, hrec.v, hrec.p);
+        }
+
+    private:
+        TexturePtr m_emit;
+    };
+
 
     class Lambertian : public Material {
     public:
@@ -216,18 +236,21 @@ namespace rayt {
             ShapeList* world = new ShapeList();
             world->add(std::make_shared<Sphere>(
                 vec3(0.6, 0, -1), 0.5f,
-                std::make_shared<Lambertian>(
-                    std::make_shared<ImageTexture>("./assets/brick_diffuse.jpg"))));
+                std::make_shared<DiffuseLight>(
+                    std::make_shared<ColorTexture>(vec3(1)))));
+
             world->add(std::make_shared<Sphere>(
                 vec3(-0.6, 0, -1), 0.5f,
                 std::make_shared<Metal>(
                     std::make_shared<ColorTexture>(vec3(0.8f, 0.8f, 0.8f)), 0.4f)));
+
             world->add(std::make_shared<Sphere>(
                 vec3(0, -100.5, -1), 100,
                 std::make_shared<Lambertian>(
                     std::make_shared<CheckerTexture>(
                         std::make_shared<ColorTexture>(vec3(0.8f, 0.8f, 0.0f)),
                         std::make_shared<ColorTexture>(vec3(0.8f, 0.2f, 0.0f)), 10.f))));
+
             m_world.reset(world);
         }
 
@@ -236,12 +259,13 @@ namespace rayt {
             HitRec hrec;
 
             if (world->hit(r, 0.001f, FLT_MAX, hrec)) {
+                vec3 emitted = hrec.mat->emitted(r, hrec);
                 ScatterRec srec;
                 if (depth < MAX_DEPTH && hrec.mat->scatter(r, hrec, srec)) {
-                    return mulPerElem(srec.albedo, color(srec.ray, world, depth + 1));
+                    return emitted + mulPerElem(srec.albedo, color(srec.ray, world, depth + 1));
                 }
                 else {
-                    return vec3(0);
+                    return emitted;
                 }
             }
             return backgroundSky(r.direction());
